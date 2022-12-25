5d122645c753ce0eb6cfbb81cdde43b62d8069b8
Mayank150397 <36702470+Mayank150397@users.noreply.github.com>
2022-12-25 20:51:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Red</h1>
+      <button>Add</button>
+      <h1>Blue</h1>
+      <button>Add</button>
+      <h1>Green</h1>
+      <button>Add</button>
+      <h1>Black</h1>
+      <button>Add</button>
+    </div>
+  );
+}
